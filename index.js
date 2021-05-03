5f26e1b3464e2a1b501db4c8329b0a73d979d1fe
Farhan Hasan <hasanfarhan33@gmail.com>
2021-05-03 13:48:25 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+let selectedColors = ['Red', 'Blue']; 
+console.log(selectedColors); 
+console.log(selectedColors.length);
+console.log(selectedColors[0]);
+console.log(selectedColors[1]);
+//The length of an array is dynamic
+selectedColors[2] = 'Green'; 
+
+//Array can store different types of variables 
+selectedColors[3] = 1; 
+
+console.log(selectedColors[2]);
+console.log(selectedColors[3]);
